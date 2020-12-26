2d6464a875a8b2c38bccbe0c07f60c1c35d0c1d0
Alfahan <ali.farhan160@gmail.com>
2020-12-26 14:47:02 +0700

    Prepare for project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 
+import 'assets/scss/style.scss';
+
 function App() {
   return (
     <div className="App">
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+// import { connect } from 'react-redux'
+
+import moduleNAme from 'elements/Button'
+
+export class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+// const mapStateToProps = (state) => ({
+    
+// })
+
+// const mapDispatchToProps = {
+    
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(LandingPage)
